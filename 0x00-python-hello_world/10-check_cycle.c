b1e39162e831a91ea9de90c6b9aec5ba0a750f9e
Ade2002 <sikirusulaimon2002@gmail.com>
2022-06-29 10:05:40 +0100

    update

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -3,9 +3,9 @@
 /**
  * check_cycle - checks if a linked list contains a cycle
  * @list: linked list to check
- *
  * Return: 1 if the list has a cycle, 0 if it doesn't
  */
+
 int check_cycle(listint_t *list)
 {
 	listint_t *slow = list;
@@ -18,18 +18,10 @@ int check_cycle(listint_t *list)
 	{
 		slow = slow->next;
 		fast = fast->next->next;
+
 		if (slow == fast)
 			return (1);
 	}
 
 	return (0);
 }
-
-
-
-100-write.py
-
-#!/usr/bin/python3
-import sys
-sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
-sys.exit(1)
