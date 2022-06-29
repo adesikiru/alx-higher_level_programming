d8bc25099cc6a4aad8db0eeb96af07a979bbdab8
Ade2002 <sikirusulaimon2002@gmail.com>
2022-06-29 10:02:52 +0100

    header file lists.h

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,5 +1,18 @@
-#ifndef A1EEF2F0_6C28_4649_A917_0346DBD3A95C
-#define A1EEF2F0_6C28_4649_A917_0346DBD3A95C
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
 
 
-#endif /* A1EEF2F0_6C28_4649_A917_0346DBD3A95C */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
+
+#endif /* LISTS_H */
