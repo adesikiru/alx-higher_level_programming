b5979d2b339ac67a49c25dd4d0bb78071503311a
Ade2002 <sikirusulaimon2002@gmail.com>
2022-10-25 09:55:00 +0100

    Task 00 a Python script that fetches 0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
+
+if __name__ == "__main__":
+
+    import urllib.request as request
+
+    with request.urlopen('https://intranet.hbtn.io/status') as response:
+        if response.readable():
+            html_request = response.read()
+            print("Body response:")
+            print("\t- type: {}".format(type(html_request)))
+            print("\t- content: {}".format(html_request))
+            print("\t- utf8 content: {}".format(html_request.decode("utf-8")))
