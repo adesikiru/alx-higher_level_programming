bb100923de6ab35e642e4350d6563a21c55eb57b
Ade2002 <sikirusulaimon2002@gmail.com>
2022-10-25 10:18:57 +0100

    Task 03 POST an email #0 3-error_code.py

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Sends a request to a URL and prints its response or error code."""
+
+import sys
+from urllib import request, error
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        url = sys.argv[1]
+        try:
+            with request.urlopen(url) as response:
+                print(response.read().decode('utf-8'))
+        except error.HTTPError as err:
+            print('Error code: {}'.format(err.code))
